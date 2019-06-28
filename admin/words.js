af8d2c9c17e40967bd59b9b099e450b3a4504f37
FeLIX <felix.mueller@harman.com>
2019-06-28 22:22:27 +0000

    add options for polling

--- admin/words.js
+++ admin/words.js
@@ -4,76 +4,34 @@
 systemDictionary = {
 	"netio adapter settings": {
 		"en": "Adapter settings for netio",
-		"de": "Adaptereinstellungen für netio",
-		"ru": "Настройки адаптера для netio",
-		"pt": "Configurações do adaptador para netio",
-		"nl": "Adapterinstellingen voor netio",
-		"fr": "Paramètres d'adaptateur pour netio",
-		"it": "Impostazioni dell'adattatore per netio",
-		"es": "Ajustes del adaptador para netio",
-		"pl": "Ustawienia adaptera dla netio",
-		"zh-cn": "netio的适配器设置"
+		"de": "Adaptereinstellungen für netio"
 	},
 	"option1": {
 		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
+		"de": "Option 1"
 	},
 	"option2": {
 		"en": "option2",
-		"de": "Option 2",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+		"de": "Option 2"
 	},
 	"username": {
 		"en": "username",
-		"de": "username",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+		"de": "username"
 	},
 	"password": {
 		"en": "password",
-		"de": "password",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+		"de": "password"
 	},
-	"ip": {
+	"netIoAddress": {
 		"en": "ip",
-		"de": "ip",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+		"de": "ip"
+	},
+	"polling": {
+		"en": "poll",
+		"de": "poll"
+	},
+	"pollingInterval": {
+		"en": "interval",
+		"de": "interval"
 	}
-	
-	
 };
